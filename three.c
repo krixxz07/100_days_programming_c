81772770d18e49333ea96ed62a9f8d0159172ccf
Krishna Jana <krishnajana662@gmail.com>
2026-08-12 00:35:14 +0530

    Add files via upload
    day 2

---
+++ three.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    float l, b, area, perimeter;
+
+    printf("enter the length of the rectangle: ");
+    scanf("%f", &l);
+    printf("enter the breadth of the rectangle: ");
+    scanf("%f", &b);
+
+    area = l * b;
+    perimeter = 2 * (l + b);
+
+    printf("Area of the rectangle: %f\n", area);
+    printf("Perimeter of the rectangle: %f\n", perimeter);
+
+    return 0;
+}
